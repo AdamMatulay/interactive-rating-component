533f25bf132e5296b2b21e9ac2c2f5f5ca74eedb
AdamMatulay <adammatulay@proton.me>
2025-11-24 14:06:28 +0100

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/interactive-rating-component/',
+  plugins: [svelte()],
+})
