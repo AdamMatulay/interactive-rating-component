0a5158b8655db3e73018c0776759253483d0a036
AdamMatulay <adammatulay@proton.me>
2025-11-26 21:03:56 +0100

    finished css

--- src/confirmation.svelte
+++ src/confirmation.svelte
@@ -1,5 +1,5 @@
 <script>
-    let { ratingResult } = $props();
+  let { ratingResult } = $props();
 </script>
 
 <div class="confirm-message">
@@ -8,8 +8,8 @@
     <p class="rating-result-text">You selected {ratingResult} out of 5</p>
   </div>
   <div class="confirm-text-content">
-    <h1>Thank you!</h1>
-    <p>
+    <h1 class="confirm-heading">Thank you!</h1>
+    <p class="confirm-text">
       We appreciate you taking the time to give a rating. If you ever need more
       support, don't hesitate to get in touch!
     </p>
@@ -17,4 +17,78 @@
 </div>
 
 <style>
+  .confirm-message {
+    max-width: 27em;
+    min-height: 27em;
+    background: linear-gradient(var(--Grey900), var(--Grey925));
+    border-radius: 20px;
+    padding: 2.2em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 2em;
+  }
+
+  .confirm-message img {
+    max-width: 10.5em;
+  }
+
+  .rating-result {
+    background: var(--Grey700);
+    border-radius: 2em;
+    padding: 0.5rem 1.25rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .rating-result-text {
+    color: var(--Orange400);
+    font-size: 1rem;
+    margin-top: 3px;
+  }
+
+  .confirm-heading {
+    font-size: 1.85rem;
+    color: var(--White);
+    margin: 0.4rem 0 0.6rem 0;
+    text-align: center;
+  }
+
+  .confirm-text {
+    font-size: 1rem;
+    color: var(--Grey500);
+    line-height: 1.6;
+    text-align: center;
+  }
+
+  @media (max-width: 26.5rem) {
+    .confirm-message {
+      max-width: 22em;
+      min-height: 24em;
+      padding: 2rem 1.5rem;
+    }
+
+    .confirm-message img {
+      max-width: 10em;
+    }
+
+    .rating-result {
+      padding: 0.3rem 0.75rem;
+    }
+
+    .rating-result-text {
+      font-size: 0.925rem;
+    }
+
+    .confirm-heading {
+      font-size: 1.5rem;
+      margin: 0rem 0 0.6rem 0;
+    }
+
+    .confirm-text {
+      font-size: 0.925rem;
+    }
+  }
 </style>
--- src/rating-form.svelte
+++ src/rating-form.svelte
@@ -1,36 +1,210 @@
 <script>
-    let { handleSubmit } = $props();
-    let selected = $state(null);
+  let { handleSubmit } = $props();
+  let selected = $state(null);
 
-    function internalSubmit(e) {
-        e.preventDefault();
-        if (handleSubmit) {
-            handleSubmit(selected);
-        }
+  function internalSubmit(e) {
+    e.preventDefault();
+    if (handleSubmit) {
+      handleSubmit(selected);
     }
+  }
 </script>
 
 <form class="rating-form" onsubmit={internalSubmit}>
-  <div>
-    <img src="./public/icon-star.svg" alt="star" />
+  <div class="star-wrapper">
+    <img class="star-image" src="./public/icon-star.svg" alt="star" />
   </div>
   <div class="rating-text-content">
-    <h1>How did we do?</h1>
-    <p>
+    <h1 class="rating-heading">How did we do?</h1>
+    <p class="rating-text">
       Please let us know how we did with your support request. All feedback is
       appreciated to help us improve our offering!
     </p>
   </div>
   <div class="rating-buttons">
-    <label><input type="radio" value="1" bind:group={selected} required /> 1</label>
-    <label><input type="radio" value="2" bind:group={selected} /> 2</label>
-    <label><input type="radio" value="3" bind:group={selected} /> 3</label>
-    <label><input type="radio" value="4" bind:group={selected} /> 4</label>
-    <label><input type="radio" value="5" bind:group={selected} /> 5</label>
+    <label class="rating-button"
+      ><input
+        type="radio"
+        value="1"
+        bind:group={selected}
+        name="rating"
+        required
+      />1</label
+    >
+    <label class="rating-button"
+      ><input
+        type="radio"
+        value="2"
+        bind:group={selected}
+        name="rating"
+      />2</label
+    >
+    <label class="rating-button"
+      ><input
+        type="radio"
+        value="3"
+        bind:group={selected}
+        name="rating"
+      />3</label
+    >
+    <label class="rating-button"
+      ><input
+        type="radio"
+        value="4"
+        bind:group={selected}
+        name="rating"
+      />4</label
+    >
+    <label class="rating-button"
+      ><input
+        type="radio"
+        value="5"
+        bind:group={selected}
+        name="rating"
+      />5</label
+    >
   </div>
   <button type="submit" id="submit">SUBMIT</button>
 </form>
 
 <style>
+  .rating-form {
+    max-width: 27em;
+    min-height: 27em;
+    background: linear-gradient(var(--Grey900), var(--Grey925));
+    border-radius: 20px;
+    padding: 2em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
 
+  .star-wrapper {
+    background: var(--Grey700);
+    width: 3em;
+    height: 3em;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .star-image {
+    width: 1.1em;
+    height: 1.1em;
+  }
+
+  .rating-heading {
+    font-size: 1.85rem;
+    color: var(--White);
+    margin-bottom: 0.5rem;
+  }
+
+  .rating-text {
+    font-size: 1rem;
+    color: var(--Grey500);
+    line-height: 1.6;
+    margin-bottom: -0.4em;
+  }
+
+  .rating-buttons {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  .rating-button input {
+    position: absolute;
+    opacity: 0;
+    width: 1px;
+    height: 1px;
+    padding: 0;
+    margin: -1px;
+    overflow: hidden;
+    clip: rect(0, 0, 0, 0);
+    border: 0;
+  }
+
+  .rating-button {
+    color: var(--Grey500);
+    background: var(--Grey700);
+    min-width: 3.4rem;
+    min-height: 3.4rem;
+    border-radius: 50%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-top: 3px;
+    font-size: 1rem;
+    font-weight: bold;
+    cursor: pointer;
+    transition: 300ms;
+  }
+
+  #submit {
+    color: var(--Grey900);
+    font-weight: 700;
+    letter-spacing: 0.1em;
+    font-size: 1.1rem;
+    background: var(--Orange500);
+    border: none;
+    outline: none;
+    border-radius: 2em;
+    padding: 0.95em 0.9em 0.9em 0.9em;
+    cursor: pointer;
+    transition: 300ms;
+  }
+
+  #submit:hover,
+  #submit:focus,
+  #submit:active {
+    background: var(--White);
+  }
+
+  .rating-button:hover {
+    background: var(--Orange500);
+    color: var(--Grey900);
+  }
+
+  .rating-button:focus-within {
+    background: var(--White);
+    color: var(--Grey900);
+  }
+
+  @media (max-width: 26.5rem) {
+    .rating-form {
+      max-width: 22em;
+      min-height: 24em;
+      padding: 2rem 1.5rem;
+    }
+
+    .star-wrapper {
+      width: 2.5em;
+      height: 2.5em;
+    }
+
+    .star-image {
+      width: 0.9em;
+      height: 0.9em;
+    }
+
+    .rating-heading {
+      font-size: 1.5rem;
+    }
+
+    .rating-text {
+      font-size: 0.925rem;
+    }
+
+    .rating-button {
+      min-width: 2.75rem;
+      min-height: 2.75rem;
+      font-size: 0.925rem;
+    }
+
+    #submit {
+      font-size: 0.9rem;
+      padding: 0.95em 0.9em 0.9em 0.9em;
+    }
+  }
 </style>
